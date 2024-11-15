3e5397e1efc93e4091909afc8fb6775fd12fcfd8
DeveloperMan313 <053000voloshch@gmail.com>
2024-11-16 00:38:55 +0300

    Add user profile base page with demonstration of file uploading

--- src/router.rs
+++ src/router.rs
@@ -1,5 +1,6 @@
 use crate::types::Request;
 use std::collections::HashMap;
+use std::fs;
 use std::net::TcpStream;
 
 use crate::response::{ext_to_content_type_enum, send_file, send_response};
@@ -51,6 +52,27 @@ fn handle_success(stream: &TcpStream) {
     );
 }
 
+fn handle_profile(stream: &TcpStream, request: &Request) {
+    if request.method == Method::Post {
+        let form_data_opt = request.parse_multipart_form();
+        // temporary for testing
+        if let Some(form_data) = form_data_opt {
+            fs::write(
+                "./image.png",
+                &form_data.get("profilePic").unwrap().field_value,
+            )
+            .ok();
+        }
+        // TODO: filter file type, save to disk with unique name and add to DB
+    }
+    send_file(
+        stream,
+        "./static/html/profile.html",
+        &ContentType::Html,
+        None,
+    );
+}
+
 pub fn route(stream: &TcpStream, request: &Request) {
     match request.path.as_str() {
         path if path.to_string().starts_with("/static/")
@@ -61,6 +83,7 @@ pub fn route(stream: &TcpStream, request: &Request) {
         "/" => handle_index(stream),
         "/register" => handle_register(stream, request),
         "/success" => handle_success(stream),
+        "/profile" => handle_profile(stream, request),
         _ => handle_not_found(stream),
     }
 }
